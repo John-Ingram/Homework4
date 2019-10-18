6f446fe0587e9ca3da9f89f88d11f0109883fb9b
John Ingram <j123j7@gmail.com>
2019-10-18 15:13:55 -0500

    Fixed logic to get info into vectors

--- Problem3.cpp
+++ Problem3.cpp
@@ -7,33 +7,33 @@
 #include <iomanip>
 #include <string>
 #include <vector>
-#include <sstream>
+#include <cstring>
 
 using namespace std;
 
-void getInfo(ifstream file,vector<string> names, vector<string> maxTemps, vector<string> minTemps);
+void getInfo(string inFileName, vector<string> &names, vector<double> &maxTemps, vector<double> &minTemps);
 
 int main(void)
 {
-	ifstream infile;
-    
+	string inFileName = ".\\weather_station_five_column.txt";
     double max, min;
-    vector<string> names, maxTemps, minTemps, maxNames, minNames;
+    vector<string> names, minNames;
+    vector<double>  maxTemps, minTemps, maxNames;
 
+    getInfo(inFileName, names, maxTemps, minTemps);
 
+    for(int i = 0; i < names.size(); i++) cout << names[i] <<"\t" << setprecision(15)<<maxTemps[i] << endl;
 	system("pause");
 	return 0;
 }
 
-void getInfo(ifstream file, vector<string> names, vector<double> maxTemps, vector<double> minTemps)
+void getInfo(string inFileName, vector<string> &names, vector<double> &maxTemps, vector<double> &minTemps)
 {
+    ifstream file;
     string identifiers [3] = {"DATE", "TMAX", "TMIN"};
     int positions [3] = {}; // Note that the first position is the end, while the others are beginnings
-    double temp;
-    char trash;
-    stringstream stream; // A stream Used to convert strings to doubles.
 	string line;
-    file.open(".\\weather_station_five_column.txt");
+    file.open(inFileName);
 	if (!file)
 	{
 		cout << "Error. Not able to open file.";
@@ -50,15 +50,13 @@ void getInfo(ifstream file, vector<string> names, vector<double> maxTemps, vecto
     while(!file.eof())
     {
         getline(file, line);
-        names.push_back(line.substr(0,positions[0])); //Add the station name to the vector
-
-        stream << line.substr(positions[1], positions[2]);// Get the max temp from the file, and put it into a stream
-        stream >> temp; // get the temperature from the stream as a doube
-        maxTemps.push_back(temp); // add that to  the vector
-
-        stream << line.substr(positions[1], string::npos); //Same as above but for max
-        stream >> temp;
-        minTemps.push_back(temp);
+        if (line.length() > positions[1])
+        {
+            names.push_back(line.substr(0,positions[0])); //Add the station name to the vector
+            maxTemps.push_back((double)atof((line.substr(positions[1], positions[2]-positions[1])).c_str())); // add max to  the vector
+            minTemps.push_back((double)atof((line.substr(positions[2], string::npos)).c_str())); // add min to  the vector
+            
+        }
     }
 }
 
