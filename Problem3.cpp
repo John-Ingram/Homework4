d88342ae045965eb65815c99f04e882b0ad1592e
John Ingram <j123j7@gmail.com>
2019-10-18 14:17:01 -0500

    Added logic to  get stuff out of the file for easier processing

--- Problem3.cpp
+++ Problem3.cpp
@@ -7,6 +7,7 @@
 #include <iomanip>
 #include <string>
 #include <vector>
+#include <sstream>
 
 using namespace std;
 
@@ -15,8 +16,7 @@ void getInfo(ifstream file,vector<string> names, vector<string> maxTemps, vector
 int main(void)
 {
 	ifstream infile;
-    char trash;
-	string line;
+    
     double max, min;
     vector<string> names, maxTemps, minTemps, maxNames, minNames;
 
@@ -25,9 +25,14 @@ int main(void)
 	return 0;
 }
 
-void getInfo(ifstream file, vector<string> names, vector<string> maxTemps, vector<string> minTemps)
+void getInfo(ifstream file, vector<string> names, vector<double> maxTemps, vector<double> minTemps)
 {
     string identifiers [3] = {"DATE", "TMAX", "TMIN"};
+    int positions [3] = {}; // Note that the first position is the end, while the others are beginnings
+    double temp;
+    char trash;
+    stringstream stream; // A stream Used to convert strings to doubles.
+	string line;
     file.open(".\\weather_station_five_column.txt");
 	if (!file)
 	{
@@ -35,9 +40,25 @@ void getInfo(ifstream file, vector<string> names, vector<string> maxTemps, vecto
 		system("pause");
 		exit(1);
 	}
+    // Set initial positions
+    getline(file, line);
+    positions[0] = line.find(identifiers[0]) - 7;
+    positions[1] = line.find(identifiers[1]);
+    positions[2] = line.find(identifiers[2]);
+
+    // Put data into vectors
     while(!file.eof())
     {
+        getline(file, line);
+        names.push_back(line.substr(0,positions[0])); //Add the station name to the vector
 
+        stream << line.substr(positions[1], positions[2]);// Get the max temp from the file, and put it into a stream
+        stream >> temp; // get the temperature from the stream as a doube
+        maxTemps.push_back(temp); // add that to  the vector
+
+        stream << line.substr(positions[1], string::npos); //Same as above but for max
+        stream >> temp;
+        minTemps.push_back(temp);
     }
 }
 
