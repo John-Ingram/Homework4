d73ec832b9c4b8c81713ba5675a2c085b978d934
T-Bunney <56650855+T-Bunney@users.noreply.github.com>
2019-10-20 15:22:16 -0500

    Add files via upload

--- Homework4.cpp
+++ Homework4.cpp
@@ -11,11 +11,13 @@ using namespace std;
 
 bool filegood(fstream &handle, string fname);
 void find_max_min(double tmax[], double tmin[], int elements, double &max_val, double &min_val);
-
+float total_precip_by_station(float prcp[], int elements, string answer, string dataline, string station[]);
+float total_precip_day(float prcp[], unsigned int days[], int elements, int DOM);
+float total_precip_range(float prcp[], unsigned int days[], int elements, int DOM, int start_day, int end_day);
 
 int main()
 {
-/*
+
 	fstream infile, outfile;
 	string infilename = "C:/Users/owner/Documents/AL_Weather_Station.txt";
 	string outfilename = "C:/Users/owner/Documents/Filtered_AL_Weather_Station.txt";
@@ -175,67 +177,12 @@ int main()
 	cout << "\n\n";
 	system("pause");
 	return 0;
-*/
 
-    fstream infile, outfile;
-    infile.open("C:/Users/owner/Documents/Filtered_AL_Weather_Station.txt");
-    outfile.open("C:/Users/owner/Documents/weather_station_five_column.txt");
-    string newline = "";
-    string s_tmax, s_tmin, s_date;
-    float tmax, tmin;
-    int pos_tmax, pos_tmin, pos_prcp, pos_mdpr, pos_dapr, pos_snwd, pos_snow, pos_awnd, counter = 0, pos_date;
-
-    getline(infile, newline);
-    newline.erase(0, 18);
-    pos_mdpr = newline.find("MDPR");
-    newline.erase(pos_mdpr, 8);
-    pos_dapr = newline.find("DAPR");
-    newline.erase(pos_dapr, 8);
-    pos_snwd = newline.find("SNWD");
-    newline.erase(pos_snwd, 8);
-    pos_snow = newline.find("SNOW");
-    newline.erase(pos_snow, 8);
-    pos_awnd = newline.find("AWND");
-    newline.erase(pos_awnd, 8);
-    pos_tmax = newline.find("TMAX");
-    pos_tmin = newline.find("TMIN");
-    pos_date = newline.find("DATE");
-    outfile << newline << endl;
-    getline(infile, newline);
-    while(!infile.eof())
-    {
-            newline.erase(0, 18);
-            newline.erase(pos_mdpr, 8);
-            newline.erase(pos_dapr, 8);
-            newline.erase(pos_snwd, 8);
-            newline.erase(pos_snow, 8);
-            newline.erase(pos_awnd, 8);
-            s_tmax = newline.substr(pos_tmax, 2);
-            s_tmin = newline.substr(pos_tmin, 2);
-            tmax = stof(s_tmax) / 10.0;
-            tmin = stof(s_tmin) / 10.0;
-            tmax = (tmax * (9 / 5)) + 32;
-            tmin = (tmin * (9 / 5)) + 32;
-            s_tmax = to_string(tmax);
-            s_tmin = to_string(tmin);
-            s_date = newline.substr(pos_date, 8);
-            s_date.insert(4," ",1);
-            s_date.insert(7," ",1);
-            newline.replace(pos_date, s_date.length(), s_date);
-            newline.replace(pos_tmax, s_tmax.length(), s_tmax);
-            newline.replace(pos_tmin, s_tmin.length(), s_tmin);
-            outfile.precision(2);
-            outfile << setprecision(2) << newline << endl;
-            counter++;
-            getline(infile, newline);
-
-    }
-    cout << counter << endl;
-    infile.close();
-    outfile.close();
-
-    return 0;
+	return 0;
 }
+
+
+
 
 void find_max_min(double tmax[], double tmin[], int elements, double &max_val, double &min_val)
 {
@@ -271,3 +218,58 @@ bool filegood(fstream &handle, string fname)
 	}
 }
 
+float total_precip_day(float prcp[], unsigned int days[], int elements, int DOM)
+{
+	float total_prcp = 0.0;
+	int k = 0;
+
+	for (k = 0; k < elements; k++)
+	{
+		if (days[k] == DOM)
+		{
+
+			total_prcp += prcp[k];
+
+		}
+
+	}
+	return total_prcp;
+}
+float total_precip_range(float prcp[], unsigned int days[], int elements, int DOM, int start_day, int end_day)
+{
+	float total_prcp = 0.0;
+	int k = 0;
+
+	for (k = 0; k < elements; k++)
+	{
+
+		if ((days[k] >= start_day) && (days[k] <= end_day))
+		{
+
+			total_prcp += prcp[k];
+
+		}
+
+	}
+	return total_prcp;
+}
+
+
+
+//Add function for part 3
+float total_precip_by_station(float prcp[], int elements, string answer, string dataline, string station[])
+{
+    string s_station;
+    float total_prcp = 0.0;
+
+    for(int i = 0; i < elements; i++)
+    {
+        if(station[i].find(answer) != string::npos)
+        {
+            total_prcp += prcp[i];
+        }
+    }
+
+    return total_prcp;
+
+}
